77305f776788a86b66dad07db91669d18c592c0b
Iwona Jastak <iwona.jastak1@gmail.com>
2018-10-22 18:09:16 +0200

    first

---
+++ desktop/src/com/my/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.my.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.my.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 480;
+		config.height = 600;
+		config.resizable = false;
+		new LwjglApplication(new Game(), config);
+	}
+}
