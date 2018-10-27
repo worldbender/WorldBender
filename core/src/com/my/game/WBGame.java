6e7b2edb1a396ca68be47f78752c6f1f88337544
arek-dabrowski <arekdbr97@gmail.com>
2018-10-27 20:09:50 +0200

    usunięcie zbędnych komentarzy.

--- core/src/com/my/game/WBGame.java
+++ core/src/com/my/game/WBGame.java
@@ -1,13 +1,6 @@
 package com.my.game;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.Array;
 import screens.SplashScreen;
 
 public class WBGame extends Game {
@@ -22,7 +15,7 @@ public class WBGame extends Game {
     public void create () {
         this.setScreen(new SplashScreen(this));
     }
-    
+
     public boolean isPaused() {
         return paused;
     }
