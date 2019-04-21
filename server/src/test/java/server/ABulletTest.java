4925da830b858180a8a3a7aa361132d9f353df3b
lzielezinska <zielezinska.lucyna@gmail.com>
2019-04-21 11:57:01 +0200

    Testing Getters And Setters in ABullet class

--- server/src/test/java/server/ABulletTest.java
+++ server/src/test/java/server/ABulletTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import server.bullets.ABullet;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 
 class ABulletTest {
@@ -14,14 +17,16 @@ class ABulletTest {
 
     @BeforeEach
     void setUp() {
-        bulletExample1 = new ABullet(1,1,30.0);
+        bulletExample1 = new ABullet(500,500,30.0);
     }
 
     @AfterEach
     void tearDown() {
         bulletExample1 = null;
     }
-
+    
+    
+/************************************************************Testing Fields**********************************************************/
     @Test
    void testIfThereIsXField() {
         assertThat(bulletExample1).hasFieldOrProperty("x");
@@ -68,7 +73,6 @@ class ABulletTest {
         assertThat(bulletExample1).hasFieldOrProperty("type");
       }
     
-    
     @Test
     void testIfThereIsBulletSpeedFieldWithProperValue() {
         
@@ -92,6 +96,119 @@ class ABulletTest {
         
         assertThat(bulletExample1).hasFieldOrProperty("hostile");
     }
+    
+    /*********************************************Testing getters and setters***************************************************************/
+
+    @Test
+    void testGetAngle() {
+        double expected = bulletExample1.getAngle();
+        double actual = 30.0;
+        assertThat(actual, is(equalTo(expected)));
+    }
+    
+    @Test
+    void testSetAngle() {
+        bulletExample1.setAngle(50);
+        double expected = bulletExample1.getAngle();
+        double actual = 50.0;
+        assertThat(actual, is(equalTo(expected)));
+    }
+    
+    @Test
+    void testGetY() {
+        double expected = bulletExample1.getY();
+        double actual = 500;
+        assertThat(actual, is(equalTo(expected)));
+    }
+    
+    @Test
+    void testSetY() {
+        bulletExample1.setY(0);
+        double expected = bulletExample1.getY();
+        double actual = 0;
+        assertThat(actual, is(equalTo(expected)));
+    }
+    
+    @Test
+    void testGetX() {
+        double expected = bulletExample1.getX();
+        double actual = 500;
+        assertThat(actual, is(equalTo(expected)));
+    }
+    
+    @Test
+    void testSetX() {
+        bulletExample1.setX(0);
+        double expected = bulletExample1.getX();
+        double actual = 0;
+        assertThat(actual, is(equalTo(expected)));
+    }
+    
+    @Test
+    void testSetAndGetId() {
+        bulletExample1.setId(1);
+        double expected = bulletExample1.getId();
+        double actual = 1;
+        assertThat(actual, is(equalTo(expected)));
+    }
+    
+    @Test
+    void testGetAndSetRange() {
+        bulletExample1.setRange(200);
+        double expected = bulletExample1.getRange();
+        double actual = 200;
+        assertThat(actual, is(equalTo(expected)));
+    }
+    
+    
+    @Test
+    void testSetAndGetType() {
+        bulletExample1.setType("Ring");
+        String expected = bulletExample1.getType();
+        String actual = "Ring";
+        assertThat(actual, is(equalTo(expected)));
+        
+    }
+    
+    
+    @Test
+    void testGetAttack() {
+        int expected = bulletExample1.getAttack();
+        int actual = 1;
+        assertThat(actual, is(equalTo(expected)));
+    }
+    
+    @Test
+    void testSetAttack() {
+        bulletExample1.setAttack(10);
+        int expected = bulletExample1.getAttack();
+        int actual = 10;
+        assertThat(actual, is(equalTo(expected)));
+    }
+    
+    @Test
+    void testIsAndSetHostile() {
+        bulletExample1.setHostile(true);
+        boolean expected = bulletExample1.isHostile();
+        boolean actual = true;
+        assertThat(actual, is(equalTo(expected)));
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     @Test
@@ -118,67 +235,4 @@ class ABulletTest {
     void handleBulletShift() {
     }
 
-    @Test
-    void getAngle() {
-    }
-
-    @Test
-    void setAngle() {
-    }
-
-    @Test
-    void getY() {
-    }
-
-    @Test
-    void setY() {
-    }
-
-    @Test
-    void getX() {
-    }
-
-    @Test
-    void setX() {
-    }
-
-    @Test
-    void getId() {
-    }
-
-    @Test
-    void setId() {
-    }
-
-    @Test
-    void getRange() {
-    }
-
-    @Test
-    void setRange() {
-    }
-
-    @Test
-    void getType() {
-    }
-
-    @Test
-    void setType() {
-    }
-
-    @Test
-    void getAttack() {
-    }
-
-    @Test
-    void setAttack() {
-    }
-
-    @Test
-    void isHostile() {
-    }
-
-    @Test
-    void setHostile() {
-    }
 }
