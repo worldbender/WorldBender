75ec1564404a5afd511aa26a72074163f5710867
lzielezinska <zielezinska.lucyna@gmail.com>
2019-05-11 10:00:14 +0200

    Creating tests in a BulletFactoryTest

--- server/src/test/java/server/BulletsFactoryTest.java
+++ server/src/test/java/server/BulletsFactoryTest.java
@@ -3,9 +3,10 @@ package server;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import server.bullets.BulletFactory;
+import server.bullets.*;
 import server.connection.GameController;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 public class BulletsFactoryTest {
@@ -20,10 +21,44 @@ public class BulletsFactoryTest {
 	
 
 	@Test
-	checkIfBulletIsATear(){
-		BulletFactory.createBullet()
+	void checkIfBulletIsATear(){
+		ABullet resultBullet = BulletFactory.createBullet("Tear", 1,1,30.0f,true, gameController);
+		assertThat(resultBullet).isInstanceOfAny(Tear.class);
 	}
 	
+	@Test
+	void checkIfBulletIsASpectralTear(){
+		ABullet resultBullet = BulletFactory.createBullet("SpectralTear", 1,1,30.0f,true, gameController);
+		assertThat(resultBullet).isInstanceOfAny(SpectralTear.class);
+	}
+	
+	@Test
+	void checkIfBulletIsAFireRing(){
+		ABullet resultBullet = BulletFactory.createBullet("FireRing", 1,1,30.0f,true, gameController);
+		assertThat(resultBullet).isInstanceOfAny(FireRing.class);
+	}
+	
+	@Test
+	void checkIfIllegalArgumentExceptionExceptionIsThrown(){
+		assertThatIllegalArgumentException().isThrownBy(() ->{
+			BulletFactory.createBullet("ShotGun", 1,1,30.0f,true, gameController);
+		}).withMessage("There is no such type of a bullet!");
+	}
+	
+	@Test
+	void checkIfNullPointerExceptionIsThrown(){
+		assertThatNullPointerException().isThrownBy(() ->{
+			BulletFactory.createBullet(null, 1,1,30.0f,true, gameController);
+		});
+	}
+	
+//	@Test
+//	void checkIfIllegalArgumentExceptionException(){
+//		assertThatIllegalArgumentException().isThrownBy(() ->{
+//			BulletFactory.createBullet("Tear", -1,-20,30.0f,true, gameController);
+//		});
+//	}
+//
 	
 	@AfterEach
 	void tearDown() {
