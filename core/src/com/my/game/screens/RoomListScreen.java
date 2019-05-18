68c5faa4d30b49a2545694a6ce87be236840e9fc
arek-dabrowski <arekdbr97@gmail.com>
2019-05-18 20:01:00 +0200

    moved init room list method + added status colors

--- core/src/com/my/game/screens/RoomListScreen.java
+++ core/src/com/my/game/screens/RoomListScreen.java
@@ -13,12 +13,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RoomListScreen extends AbstractScreen {
+    private List<Room> rooms;
     private ScrollPane scrollPane;
     private float gameWidth, gameHeight;
     private int bound = 400;
 
     public RoomListScreen(WBGame game) {
         super(game);
+        rooms = new ArrayList<>();
+
+        fillFakeRooms();
     }
 
     @Override
@@ -44,51 +48,17 @@ public class RoomListScreen extends AbstractScreen {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 System.out.println("REFRESHED!");
+                game.changeScreen(WBGame.ROOM_LIST);
             }
         });
 
         gameWidth = Gdx.graphics.getWidth();
         gameHeight = Gdx.graphics.getHeight();
 
-        Table listTable = new Table();
+        Table roomList = new Table();
+        initRoomList(roomList);
 
-        Room[] rooms = new Room[100];
-
-        listTable.add(new Label("Room ID", skin, "white")).left().expandX().fillX();
-        listTable.add(new Label("Room owner", skin, "white")).center().expandX().fillX();
-        listTable.add(new Label("Players in room", skin, "white")).center().expandX().fillX();
-        listTable.add(new Label("Game status", skin, "white")).right().expandX().fillX();
-        listTable.add(new Label("", skin, "white")).expandX().fill();
-        listTable.row();
-
-        for (int i = 0, k = 0; i < 100; i++) {
-            final int id = i;
-            rooms[k++] = new Room(i, "Ziutek" + 2*i, i%5);
-            final TextButton joinButton = new TextButton("Join", skin, "default");
-            joinButton.addListener(new ChangeListener() {
-                @Override
-                public void changed(ChangeEvent event, Actor actor) {
-                    WBGame.getConnection().getTcp().sendMessage(new JSONObject()
-                            .put("msg", "joinRoom")
-                            .put("content", new JSONObject().put("id", id))
-                            .put("character", "Ground")
-                    );
-                }
-            });
-            if(!rooms[i].canPlayerJoin()) {
-                joinButton.setDisabled(true);
-                joinButton.setTouchable(Touchable.disabled);
-            }
-            listTable.add(new Label(rooms[i].getRoomId(), skin, "medium")).left().expandX().fillX();
-            listTable.add(new Label(rooms[i].getRoomOwner(), skin, "medium")).center().expandX().fillX();
-            listTable.add(new Label(rooms[i].getPlayersInRoom(), skin, "medium")).center().expandX().fillX();
-            if(rooms[i].canPlayerJoin()) listTable.add(new Label(rooms[i].getGameStatus(), skin, "medium")).right().expandX().fillX();
-            else listTable.add(new Label(rooms[i].getGameStatus(), skin, "medium-white")).right().expandX().fillX();
-            listTable.add(joinButton).expandX().fill();
-            listTable.row();
-        }
-
-        scrollPane = new ScrollPane(listTable, skin);
+        scrollPane = new ScrollPane(roomList, skin);
         scrollPane.setFadeScrollBars(false);
         scrollPane.setBounds(bound, bound/2, gameWidth - 2*bound, gameHeight- bound);
         scrollPane.setSmoothScrolling(false);
@@ -129,4 +99,46 @@ public class RoomListScreen extends AbstractScreen {
         stage.dispose();
     }
 
+    private void initRoomList(Table roomList){
+        roomList.add(new Label("Room ID", skin, "white")).left().expandX().fillX();
+        roomList.add(new Label("Room owner", skin, "white")).center().expandX().fillX();
+        roomList.add(new Label("Players in room", skin, "white")).center().expandX().fillX();
+        roomList.add(new Label("Game status", skin, "white")).right().expandX().fillX();
+        roomList.add(new Label("", skin, "white")).expandX().fill();
+        roomList.row();
+
+        //TODO: character injection
+        for(Room room : rooms){
+            final int id = Integer.parseInt(room.getRoomId());
+            TextButton joinButton = new TextButton("Join", skin, "default");
+
+            joinButton.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+                    WBGame.getConnection().getTcp().sendMessage(new JSONObject()
+                            .put("msg", "joinRoom")
+                            .put("content", new JSONObject().put("id", id))
+                            .put("character", "Ground")
+                    );
+                }
+            });
+            roomList.add(new Label(room.getRoomId(), skin, "medium")).left().expandX().fillX();
+            roomList.add(new Label(room.getRoomOwner(), skin, "medium")).center().expandX().fillX();
+            roomList.add(new Label(room.getPlayersInRoom(), skin, "medium")).center().expandX().fillX();
+            if(!room.canPlayerJoin()) {
+                joinButton.setDisabled(true);
+                joinButton.setTouchable(Touchable.disabled);
+                roomList.add(new Label(room.getGameStatus(), skin, "medium-red")).right().expandX().fillX();
+            } else roomList.add(new Label(room.getGameStatus(), skin, "medium-green")).right().expandX().fillX();
+            roomList.add(joinButton).expandX().fill();
+            roomList.row();
+        }
+    }
+
+    //TODO: temporal method
+    private void fillFakeRooms(){
+        for (int i = 0; i < 100; i++) {
+            rooms.add(new Room(i, "Ziutek" + 2*i, i%5));
+        }
+    }
 }
