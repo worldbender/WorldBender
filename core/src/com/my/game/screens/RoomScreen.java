cb03fce721ea1b4e3e3db603738b2507cb024cfc
arek-dabrowski <arekdbr97@gmail.com>
2019-05-22 00:15:23 +0200

    methods refactor

--- core/src/com/my/game/screens/RoomScreen.java
+++ core/src/com/my/game/screens/RoomScreen.java
@@ -1,16 +1,13 @@
 package com.my.game.screens;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.my.game.MyAssetManager;
 import com.my.game.WBGame;
-import com.my.game.player.Player;
 import org.json.JSONObject;
 
 import static com.my.game.MyAssetManager.*;
@@ -19,12 +16,18 @@ public class RoomScreen extends AbstractScreen {
     private boolean isOwner = false;
     private int roomId;
     private ScrollPane scrollPane;
+    private Table playerCards;
     private float gameWidth, gameHeight;
     private int boundRatio = 400;
+    private SelectBox selectPlayerCharacter;
+
 
     public RoomScreen(WBGame game, int roomId) {
         super(game);
         this.roomId = roomId;
+        playerCards = new Table(skin);
+        selectPlayerCharacter = new SelectBox(skin);
+        selectPlayerCharacter.setItems("Ground", "Water");
     }
 
     public RoomScreen(WBGame game, boolean isOwner, int roomId) {
@@ -77,20 +80,12 @@ public class RoomScreen extends AbstractScreen {
         gameWidth = WBGame.WIDTH;
         gameHeight = WBGame.HEIGHT;
 
-        Table players = new Table(skin);
-        players.align(Align.top);
+        playerCards.align(Align.top);
 
-        initPlayer(players, "player1", PROF_ROOM);
-        initPlayer(players, "player2", BLOND_ROOM);
-        initPlayer(players, "player3", PROF_ROOM);
-        initPlayer(players, "player4", BLOND_ROOM);
-        players.row();
+        initPlayers();
 
-        scrollPane = new ScrollPane(players, skin, "no-bg");
-        scrollPane.setFadeScrollBars(false);
-        scrollPane.setBounds(boundRatio - 100, boundRatio/2, gameWidth - 2*boundRatio + 200, gameHeight - boundRatio);
-//        scrollPane.setBounds(boundRatio, boundRatio/2, gameWidth - 2*boundRatio, gameHeight - boundRatio);
-        scrollPane.setSmoothScrolling(false);
+        scrollPane = new ScrollPane(playerCards, skin, "no-bg");
+        scrollPane.setBounds(boundRatio - 100, boundRatio/2 - 50, gameWidth - 2*boundRatio + 200, gameHeight - boundRatio);
         scrollPane.setTransform(true);
 
         stage.addActor(scrollPane);
@@ -133,29 +128,41 @@ public class RoomScreen extends AbstractScreen {
         stage.dispose();
     }
 
-    private void initPlayer(Table players, String player, String type){
+    private void initPlayers(){
+        initPlayer("player1", PROF_ROOM);
+        initPlayer("player2", BLOND_ROOM);
+        initPlayer("player3", PROF_ROOM);
+        initPlayer("player4", BLOND_ROOM);
+        playerCards.row();
+    }
+
+    private void initPlayer(String playerx, String type){
         SelectBox selectCharacter = new SelectBox(skin);
         selectCharacter.setItems("Ground", "Water");
+        selectCharacter.setSelected("Water");
+        selectCharacter.setDisabled(true);
 
-        Table subtable = new Table(skin);
-        subtable.setBackground("file-menu-bar");
+        Table playerCard = new Table(skin);
+        playerCard.setBackground("file-menu-bar");
 
-        Label p1 = new Label(player, skin, "white");
-        p1.setAlignment(Align.center);
-        subtable.add(p1).width(200).padTop(10).padBottom(20).expandX().fillX();
-        subtable.row().expandX().fillX();
+        Label player = new Label(playerx, skin, "title-white");
+        player.setFontScale(0.75f);
+        player.setAlignment(Align.center);
+        playerCard.add(player).width(200).padTop(10).padBottom(20).expandX().fillX();
+        playerCard.row().expandX().fillX();
+
         Texture texture = MyAssetManager.manager.get(type);
-        TextureRegion playerHeadTexture = new TextureRegion(Player.headRegion);
-//        subtable.add(new Image(new TextureRegionDrawable(playerHeadTexture))).width(200).height(200);
-        subtable.add(new Image(texture)).width(200).height(230).padBottom(20);
-        subtable.row().expandX().fillX();
-        Label characterClass = new Label("Character Class:", skin, "white");
-        characterClass.setAlignment(Align.center);
-        subtable.add(characterClass).padBottom(10).expandX().fillX();
-        subtable.row().expandX().fillX();
-        subtable.add(selectCharacter).width(180).padBottom(20);
-        subtable.row().expandX().fillX();
+        playerCard.add(new Image(texture)).height(230).padBottom(20);
+        playerCard.row().expandX().fillX();
 
-        players.add(subtable).pad(10);
+        Label characterClass = new Label("Character Class:", skin, "default");
+        characterClass.setAlignment(Align.center);
+        playerCard.add(characterClass).padBottom(10).expandX().fillX();
+        playerCard.row().expandX().fillX();
+
+        playerCard.add(selectCharacter).width(180).padBottom(20);
+        playerCard.row().expandX().fillX();
+
+        playerCards.add(playerCard).pad(10);
     }
 }
